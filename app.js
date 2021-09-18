063b5945f1beb93b2f50610c34ff0e04a8346010
Elena <elenasoto23@gmail.com>
2021-09-18 20:56:19 -0300

    repoMercadoLiebreGitHub

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+/*const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));*/
+const path = require('path');
+app.use(express.static('public'));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.get("/login", (req,res) =>{
+    res.sendFile(path.resolve("./views/home.html"));
+});
+
+
+app.listen( process.env.PORT ||3008,() => console.log ("servidor en puerto 3008"))
