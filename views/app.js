df5950253350b79291e4fe5bac533470331e0a8c
Elena <elenasoto23@gmail.com>
2021-09-19 18:32:20 -0300

    loginmercadoliebre

--- views/app.js
+++ views/app.js
@@ -9,9 +9,12 @@ app.use(express.static('public'));
 app.get("/", (req,res) => {
     res.sendFile(path.resolve("./views/home.html"));
 });
+app.get("/register", (req,res) =>{
+    res.sendFile(path.resolve("./views/register.html"));
+});
 
 app.get("/login", (req,res) =>{
-    res.sendFile(path.resolve("./views/home.html"));
+    res.sendFile(path.resolve("./views/login.html"));
 });
 
 
